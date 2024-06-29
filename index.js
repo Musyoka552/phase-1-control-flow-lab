efc68dd1072135da0ee6f9af40781ac9711d40e2
Musyoka552 <kilonzopatoh6@gmail.com>
2024-06-29 11:52:13 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+  if(num===199 && num<400)
+  { return "This one is on me!"
+  }
+  if(num===1500)
+    {
+      return"That will be twenty bucks."
+    }
+    if(num===2001)
+      {
+        return "I will gladly take your thirty bucks."
+      }
+      if(num===2501)
+      {
+        return "No can do."
+      }
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return 'Ok, sounds good.'
+
+  }
+  if(city === 'Pittsburgh'){
+    return 'No go.'
+  }
+
   // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return 'Thank you so much.'
+  }
+  if(tip != 'generous'){
+    return 'Thank you.'
+  }
+  if(tip = 'Anything'){
+    return 'Bye.'
+  }
+
   // Write your code here!
 }
